9fc0857bf2603f58dfc1e21ea62ae0d22df599a9
aleks <aleks@PK>
2021-02-13 18:06:48 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# external
-Get external IP address
+Description
+===========
+
+	Get external IP address
+
+Requests
+========
+	OS: UNIX/LINUX/Windows
+
+install
+=======
+
+	git clone https://github.com/ErilinAleksei/external.git
+	sudo apt install python2
+
+Using
+=====
+
+	python2 external.py
+	
+Contacts
+========
+
+	email: makentosh1@mail.ru
+	URL: https://github.com/ErilinAleksei
---
+++ external.py
@@ -0,0 +1,25 @@
+import urllib
+import re
+
+URL = "https://2ip.ru"
+
+def getExternalIP():
+    try:
+        resp = urllib.urlopen(URL)
+        if resp.code == 200:
+            data = resp.read()
+            reg_ip = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+            s = reg_ip.search(data)
+            return data[s.start():s.end()]
+    except Exception as e:
+        print(e)
+        return None
+    
+def main():
+    ipaddr = getExternalIP()
+    if ipaddr != None :
+        print("External IP: %s" % ipaddr)
+        
+
+if __name__ == "__main__":
+    main()
